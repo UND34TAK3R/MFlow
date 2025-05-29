41c516a2d8fb0059c33b2fa93a9ede269be418c5
Derrick Mangari <dmangari@live.fr>
2025-05-28 23:19:39 -0400

    Initial commit for MFlow Project

---
+++ backend/mflow/src/main/java/com/mdev/mflow/MflowApplication.java
@@ -0,0 +1,13 @@
+package com.mdev.mflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MflowApplication.class, args);
+    }
+
+}
---
+++ frontend/android/app/src/main/java/com/mdev/frontend/MainActivity.java
@@ -0,0 +1,6 @@
+package com.mdev.frontend;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
